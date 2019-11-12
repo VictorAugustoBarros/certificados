4a64bcf98089b4c3a596c21544e6c79756a095ad
victor <victor.barros@pgmais.com.br>
2019-11-12 18:15:32 -0300

    ok

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <router-link to="/index" class="nav-link">Home</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/about" class="nav-link">About</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/certificados" class="nav-link">Certificados</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/usuarios" class="nav-link">Usuarios</router-link>
-                </li>
-                <li class="nav-item">
-                    <mdb-dropdown>
-                        <mdb-dropdown-toggle slot="toggle">Basic dropdown</mdb-dropdown-toggle>
-                        <mdb-dropdown-menu>
-                            <mdb-dropdown-item>Action</mdb-dropdown-item>
-                            <mdb-dropdown-item>Another action</mdb-dropdown-item>
-                            <mdb-dropdown-item>Something else here</mdb-dropdown-item>
-                            <div class="dropdown-divider"></div>
-                            <mdb-dropdown-item>Separated link</mdb-dropdown-item>
-                        </mdb-dropdown-menu>
-                    </mdb-dropdown>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/logout" class="nav-link">Logout</router-link>
-                </li>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Certificados</b-navbar-brand>
 
-            </ul>
-        </nav>
-        <br/>
-        <transition name="fade">
-            <router-view></router-view>
-        </transition>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item href="#">Link</b-nav-item>
+                    <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                    </b-nav-form>
+
+                    <b-nav-item-dropdown text="Lang" right>
+                        <b-dropdown-item href="#">EN</b-dropdown-item>
+                        <b-dropdown-item href="#">ES</b-dropdown-item>
+                        <b-dropdown-item href="#">RU</b-dropdown-item>
+                        <b-dropdown-item href="#">FA</b-dropdown-item>
+                    </b-nav-item-dropdown>
+
+                    <b-nav-item-dropdown right>
+                        <!-- Using 'button-content' slot -->
+                        <template v-slot:button-content>
+                            <em>User</em>
+                        </template>
+                        <b-dropdown-item href="#">Profile</b-dropdown-item>
+                        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
     </div>
 </template>
 
@@ -55,15 +55,9 @@
 </style>
 
 <script>
-    import {mdbDropdown, mdbDropdownItem, mdbDropdownMenu, mdbDropdownToggle} from 'mdbvue';
 
     export default {
         name: 'DropdownPage',
-        components: {
-            mdbDropdown,
-            mdbDropdownItem,
-            mdbDropdownMenu,
-            mdbDropdownToggle
-        }
+        components: {}
     }
 </script>
